2433c90f2a73bc6708d6459d168b539f0a7cee3c
Anastasiya <nastya.eremenko@mail.ru>
2021-06-29 21:05:16 +0300

    Actions check test and Docker v3

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
+        'NAME': 'postgres',
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
